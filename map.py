40bd26b73ccd9ece4a3263c583e58d185c041297
ernest404 <ernestmuisyoo@gmail.com>
2022-09-11 23:47:00 +0300

    Testing maps in python

---
+++ map.py
@@ -0,0 +1,17 @@
+from ipywidgets import embed
+import streamlit.components.v1 as components
+import gmaps
+gmaps.configure(api_key = 'AIzaSyBwMwFNtGh9etlN-dmusTNQQO1bGIK1Mac')
+
+def new_york():
+
+    # Plot coordinates
+    coordinates = (40.75, -74)
+    _map = gmaps.figure(center=coordinates, zoom_level=12)
+
+    # Render map in Streamlit
+    snippet = embed.embed_snippet(views=_map)
+    html = embed.html_template.format(title="", snippet=snippet)
+    return components.html(html, height=500,width=500)
+
+new_york()
